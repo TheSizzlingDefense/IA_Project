9a01610020e4bdf56ff43b5c90998ae3470b3909
TheSizzlingDefense <vladislavtruhanov4@gmail.com>
2025-11-21 10:47:31 -0500

    Added second dialog

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "deckwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -12,3 +13,11 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_access_decks_clicked()
+{
+    DeckWindow deckwindow;
+    deckwindow.setModal(true);
+    deckwindow.exec();
+}
+
